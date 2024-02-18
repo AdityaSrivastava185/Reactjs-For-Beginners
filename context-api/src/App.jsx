eac96a52d806286fcb61a20de6693a2f417c5fa6
Aditya-Srivastava <pks85715@gmail.com>
2024-02-18 12:52:23 +0530

    remove file

---
+++ context-api/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import UserContextProvider from './Context/UserContextProvider'
+import Login from './Components/Login'
+import Profile from './Components/Profile'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <UserContextProvider>
+      <Login/>
+      <Profile/>
+    </UserContextProvider>
+  )
+}
+
+export default App
