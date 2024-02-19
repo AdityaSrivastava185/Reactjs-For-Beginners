01b3a1b1229579921491cf12a3b44e09e213f86c
Aditya-Srivastava <pks85715@gmail.com>
2024-02-19 20:39:52 +0530

    create first component

---
+++ basicreact01/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Hello from './Components/Hello'
+function App() {
+
+  return (
+    <>
+      <h1>Chai aur react</h1>
+      <Hello/>
+    </>
+  )
+}
+
+export default App
