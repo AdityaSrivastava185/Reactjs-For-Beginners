ef864b1f205b53b24a669f9242c8a0eec72fad37
Aditya Srivastava <102461506+AdityaSrivastava185@users.noreply.github.com>
2024-03-19 00:27:15 +0530

    Delete MegaBlog directory

--- MegaBlog/src/conf/conf.js
+++
@@ -1,9 +0,0 @@
-const conf = {
-    appwriteurl : String(import.meta.env.VITE_APPWRITE_URL),
-    projectid : String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
-    databaseid : String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
-    collectionid : String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
-    bucketid : String(import.meta.env.VITE_APPWRITE_BUCKET_ID)
-}
-
-export default conf
